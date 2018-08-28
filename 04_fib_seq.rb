e00f19110f9df301320737052e9ca291b4ee818a
kieran-lockyer <kieran.lockyer.87@gmail.com>
2018-08-29 09:10:06 +1000

    FIbonacci morning challenge destroyed

--- 04_fib_seq.rb
+++ 04_fib_seq.rb
@@ -1,3 +1,13 @@
 # 1. sit beside someone new, at a new spot :slightly_smiling_face:
 
 # 2. There is an odd employee at The Company who needs some code written that will provide them with the first 100 numbers of the Fibonacci sequence ….. (no one really knows why the odd employee needs this information - or why they can’t work it out themselves - but Monday mornings can be strange).
+
+fibonacci = [1, 1]
+
+while fibonacci.count() < 100
+    fibonacci.push(fibonacci[-1] + fibonacci[-2])
+end
+
+100.times do |num|
+    puts "#{num + 1}. #{fibonacci[num]}"
+end
