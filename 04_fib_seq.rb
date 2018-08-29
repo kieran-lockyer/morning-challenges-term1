f2caba9452f22defba915bdc191f4ce60c7c7190
kieran-lockyer <kieran.lockyer.87@gmail.com>
2018-08-29 10:17:10 +1000

    Added Iterative and Recursive functions to the Fibonacci challenge

--- 04_fib_seq.rb
+++ 04_fib_seq.rb
@@ -2,12 +2,25 @@
 
 # 2. There is an odd employee at The Company who needs some code written that will provide them with the first 100 numbers of the Fibonacci sequence ….. (no one really knows why the odd employee needs this information - or why they can’t work it out themselves - but Monday mornings can be strange).
 
-fibonacci = [1, 1]
+# def fib_iter(num)
+#     fibonacci = [1, 1]
 
-while fibonacci.count() < 100
-    fibonacci.push(fibonacci[-1] + fibonacci[-2])
+#     while fibonacci.count() < num
+#         fibonacci.push(fibonacci[-1] + fibonacci[-2])
+#     end
+
+#     num.times do |item|
+#         puts "#{item + 1}. #{fibonacci[item]}"
+#     end
+# end
+
+def fib_rec(num, arr = [1,1])
+    if arr.count() == num
+        return arr
+    end
+    fib_rec(num, arr.push(arr[-1] + arr[-2]))
 end
 
-100.times do |num|
-    puts "#{num + 1}. #{fibonacci[num]}"
-end
+# fib_iter(100)
+fibonacci = fib_rec(100)
+fibonacci.each_index {|fib| puts  "#{fib + 1}. #{fibonacci[fib]}"}
