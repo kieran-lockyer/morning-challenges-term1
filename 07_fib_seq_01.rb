3e9f59b8754238083c56a9e19fdc040b67bc4ab7
kieran-lockyer <kieran.lockyer.87@gmail.com>
2018-09-03 09:55:48 +1000

    completed morning challenge)

--- 07_fib_seq_01.rb
+++ 07_fib_seq_01.rb
@@ -1,8 +1,9 @@
 # A slightly different kind of challenge this morning.
 
-# The Company has broken the keyboard on their one and only laptop, so can’t get this bit of code to run. What would the output of this code be (without running it on your own computer)? 
+# The Company has broken the keyboard on their one and only laptop, so can’t get this bit of code to run. 
+# What would the output of this code be (without running it on your own computer)? 
 
-#  def fib(num_
+#  def fib(num)
 #   if num < 2
 #      num
 #   else
@@ -10,3 +11,14 @@
 #   end
 # end
 # puts fib(4)
+
+#it would put out 1, 0, -1 and -2 a bunch of times.
+
+ def fib(num)
+  if num < 2
+     num
+  else
+     fib(num-1) + fib(num-2)
+  end
+end
+puts fib(4)
