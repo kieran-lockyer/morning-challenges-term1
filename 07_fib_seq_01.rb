408a50389392fb02ed29919cf82c70f5b8007d4d
kieran-lockyer <kieran.lockyer.87@gmail.com>
2018-09-06 10:50:42 +1000

    completed challenge

--- 07_fib_seq_01.rb
+++ 07_fib_seq_01.rb
@@ -12,13 +12,15 @@
 # end
 # puts fib(4)
 
-#it would put out 1, 0, -1 and -2 a bunch of times.
+# it would put out 1, 0, -1 and -2 a bunch of times.
 
- def fib(num)
-  if num < 2
-     num
-  else
-     fib(num-1) + fib(num-2)
-  end
+def fib(num)
+    if num < 2
+        num
+    else
+        fib(num-1) + fib(num-2)
+    end
 end
 puts fib(4)
+
+# Turns out it puts out 3, as it's return the output of recursive fib(num-1) + fib(num-2). My error was thinking out would only return if it was less than 2.
