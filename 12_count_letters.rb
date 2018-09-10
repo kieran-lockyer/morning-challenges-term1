5ebeb4f17d6b04ec81c3b059a1d1651d1337ba8b
kieran-lockyer <kieran.lockyer.87@gmail.com>
2018-09-11 09:54:30 +1000

    refactored to if from unless for readability and added tests folder

--- 12_count_letters.rb
+++ 12_count_letters.rb
@@ -17,7 +17,16 @@
 def count_letters (string)
   result = {} # You'll need an empty hash to get started!
   
-  # Your code here
+  for letter in string.each_char do
+    if result[letter]
+      result[letter] += 1
+    else
+      result[letter] = 1
+    end
+    
+  end
 
   result # return the hash
 end
+
+puts count_letters("hello")
